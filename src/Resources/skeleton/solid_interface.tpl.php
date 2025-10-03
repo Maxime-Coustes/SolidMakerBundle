8f7ee9003d38973b012a64e0eecbd4d9391ab5c4
Maxime-Coustes <maxime.coustes@hotmail.fr>
2025-10-03 15:49:35 +0200

    update the interface to be more accurate

--- src/Resources/skeleton/solid_interface.tpl.php
+++ src/Resources/skeleton/solid_interface.tpl.php
@@ -7,28 +7,35 @@ use App\Entity\<?= $entityName ?>Collection;
 
 interface <?= $interfaceName ?>
 {
-/**
+    /**
      * Crée de nouvelles entités <?= basename(str_replace('\\', '/', $entityName)) ?> à partir d'une collection.
      *
      * @param <?= basename(str_replace('\\', '/', $entityName)) ?>Collection $<?= lcfirst(basename(str_replace('\\', '/', $entityName))) ?>Collection
-     * @return array{created: <?= $entityName ?>Collection ?>, existing: <?= $entityName ?>Collection ?>}
+     * @return array{
+     *     created: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection, 
+     *     existing: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection
+     * }
      */
     public function create<?= $entityName ?>Collection(<?= basename(str_replace('\\', '/', $entityName)) ?>Collection $<?= lcfirst(basename(str_replace('\\', '/', $entityName))) ?>Collection): array;
 
     /**
-    * Met à jour une collection d'entités <?= basename(str_replace('\\', '/', $entityName)) ?>.
-    *
-    * @param <?= basename(str_replace('\\', '/', $entityName)) ?>Collection $<?= lcfirst(basename(str_replace('\\', '/', $entityName))) ?>Collection
-    * @return array{updated: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection, not_found: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection}
-    */
+     * Met à jour une collection d'entités <?= basename(str_replace('\\', '/', $entityName)) ?>.
+     *
+     * Chaque entité de la collection est comparée à l'existante en base. 
+     * Les entités mises à jour sont renvoyées, ainsi que celles non trouvées.
+     *
+     * @param <?= basename(str_replace('\\', '/', $entityName)) ?>Collection $<?= lcfirst(basename(str_replace('\\', '/', $entityName))) ?>Collection
+     * @return array{
+     *     updated: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection,
+     *     not_found: <?= basename(str_replace('\\', '/', $entityName)) ?>Collection
+     * }
+     */
     public function update<?= basename(str_replace('\\', '/', $entityName)) ?>s(<?= basename(str_replace('\\', '/', $entityName)) ?>Collection $<?= lcfirst(basename(str_replace('\\', '/', $entityName))) ?>Collection): array;
-
 
     /**
      * Supprime une entité <?= basename(str_replace('\\', '/', $entityName)) ?> par son ID.
      *
      * @param int $id
-     *
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException Si l'entité n'existe pas
      */
     public function delete<?= basename(str_replace('\\', '/', $entityName)) ?>ById(int $id): void;
